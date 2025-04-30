22d3d8644bb9205f09d0d3e0b1c8c8acd995f3e9
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-04-30 15:08:16 -0300

    implementa teste para criacao de membro

--- tests/member.test.ts
+++ tests/member.test.ts
@@ -1,11 +1,30 @@
+import MemberRepository from '@base/repositories/member-repository'
+import IRepository from '@base/repositories/repository'
+import CreateMemberService from '@base/services/create-member-service'
 import assert from 'assert'
-import { describe, it } from 'node:test'
+import { describe, it, afterEach } from 'node:test'
 
 describe('members suite tests', () => {
   describe('create members', () => {
     describe('sucess cases', () => {
-      it('Initial tests', () => {
-        assert.strictEqual(true, true)
+      it('Should create a member', async () => {
+        const expectedResult = { id: '123' }
+        const mockRepo: IRepository = {
+          save: () => Promise.resolve({ _id: expectedResult.id })
+        }
+        const createMemberService = new CreateMemberService(mockRepo)
+        const birthday = new Date()
+        const data = {
+          name: 'Sunda Foo',
+          username: 'sunda',
+          birthday,
+          type: 'user',
+          groupId: '456',
+          password: '123',
+          photo: ''
+        }
+        const result = await createMemberService.execute(data)
+        assert.deepStrictEqual(result, expectedResult)
       })
     })
   })
