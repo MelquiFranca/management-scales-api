bd9b88172a2ad8e1cba4df4eed73672d8d904b9a
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-04-30 10:13:58 -0300

    configura primeiro teste

---
+++ tests/member.test.ts
@@ -0,0 +1,12 @@
+import assert from 'assert'
+import { describe, it } from 'node:test'
+
+describe('members suite tests', () => {
+  describe('create members', () => {
+    describe('sucess cases', () => {
+      it('Initial tests', () => {
+        assert.strictEqual(true, true)
+      })
+    })
+  })
+})
