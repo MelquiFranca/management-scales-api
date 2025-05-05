8739dc3934443a765bfbcff18251493c540fbbd1
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-05-05 10:51:32 -0300

    cria uma instancia inicial do servidor e expoe rotas de get e post

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello world')
+
+import MemberController from './controllers/member-controller'
+import Database from './infra/database'
+import app from './infra/server'
+
+(async () => {
+  const uri = 'mongodb://localhost:27017'
+  const databaseName = 'management-squad'
+  const port = 8002
+  const database = new Database(uri, databaseName)
+  await database.connect()
+  const memberControler = new MemberController(database)
+  app.post(`/${memberControler.path}`, memberControler.create.bind(memberControler))
+  app.get(`/${memberControler.path}`, memberControler.list.bind(memberControler))
+
+  app.listen(port, () => console.log(`Running in port ${port}`))
+  process.addListener('SIGTERM', async () => {
+    await database.disconnect()
+  })
+})()
