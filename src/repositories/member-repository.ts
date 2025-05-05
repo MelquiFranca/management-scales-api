5a13dcdc29bf987e6636f1489b57d9023080afeb
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-05-05 12:13:08 -0300

    simplifica escrita

--- src/repositories/member-repository.ts
+++ src/repositories/member-repository.ts
@@ -1,16 +1,15 @@
 import Member from '@base/entities/member'
 import IRepository from './repository'
 import { DTOFilter, DTOMember, DTORepositoryResult } from '@base/dtos'
-import Database from '@base/infra/database'
 import IDatabase from '@base/infra/idatabase'
 
 export default class MemberRepository implements IRepository {
-  #collectionName: string
-  constructor (private readonly database: IDatabase, collectionName: string) {
-    this.#collectionName = collectionName
-  }
+  constructor (
+    private readonly database: IDatabase, 
+    private readonly collectionName: string
+  ) {}
   async save (data: DTOMember): Promise<DTORepositoryResult> {
-    return this.database.save(this.#collectionName, data)
+    return this.database.save(this.collectionName, data)
   }
   async remove(id: string): Promise<DTORepositoryResult> {
     return Promise.resolve({ id, removed: true })
@@ -19,7 +18,7 @@ export default class MemberRepository implements IRepository {
     return Promise.resolve({ id: data.id, updated: true })
   }
   async list<DTOMember>(filter: DTOFilter): Promise<DTOMember[]> {
-    const members = await this.database.list<DTOMember>(this.#collectionName, filter)
+    const members = await this.database.list<DTOMember>(this.collectionName, filter)
     return members
   }
 }
