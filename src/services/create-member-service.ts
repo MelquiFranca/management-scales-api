406095ebd3c79183883f05af48ad59d674b7c029
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-05-06 16:06:15 -0300

    adiciona metodo para encriptar password em create member

--- src/services/create-member-service.ts
+++ src/services/create-member-service.ts
@@ -1,12 +1,12 @@
-import crypto, { Hash } from 'crypto'
+import crypto from 'crypto'
 import { DTOCreateMember, DTORepositoryResult } from '@base/dtos'
 import IRepository from '@base/repositories/repository'
 
 export default class CreateMemberService {
   constructor (private readonly repository: IRepository) {}
   #cryptPassword (password: string): string {
-    return password
-  }
+      return crypto.scryptSync(password, 'salt', 64).toString('hex');
+    }
   async execute (data: DTOCreateMember): Promise<DTORepositoryResult> {
     if (data.password?.length < 3) throw new Error('Invalid password')
     if (data.name?.length < 3) throw new Error('Invalid name')
