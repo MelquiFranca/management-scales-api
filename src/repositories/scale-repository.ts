6bd9706e4d4616355bdd9515a719f0be247bb575
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-05-07 19:43:26 -0300

    cria repository para escala

---
+++ src/repositories/scale-repository.ts
@@ -0,0 +1,26 @@
+import IRepository from './repository'
+import { DTOFilter, DTORepositoryResult, DTOScale } from '@base/dtos'
+import IDatabase from '@base/infra/idatabase'
+import Scale from '@base/entities/scale'
+
+export default class ScaleRepository implements IRepository {
+  constructor (
+    private readonly database: IDatabase,
+    private readonly collectionName: string = 'scales'
+  ) {}
+  async save (data: DTOScale): Promise<DTORepositoryResult> {
+    return this.database.save(this.collectionName, data)
+  }
+  async remove(id: string, filter?: Object): Promise<DTORepositoryResult> {
+    return this.database.remove(this.collectionName, id, filter)
+  }
+  async update(data: Scale): Promise<DTORepositoryResult> {
+    return Promise.resolve({ id: data.id, updated: true })
+  }
+  async list<DTOScale>(filter: DTOFilter): Promise<DTOScale[]> {
+    return this.database.list<DTOScale>(this.collectionName, filter)
+  }
+  async findByQuery<DTOScale>(filter: DTOFilter, options?: Object): Promise<DTOScale|null> {
+    return this.database.find<DTOScale|null>(this.collectionName, filter, options)
+  }
+}
