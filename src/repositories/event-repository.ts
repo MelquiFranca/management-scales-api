89c951c0e0d302abb8b7ccf9693bbc33c60fd070
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-05-07 17:14:21 -0300

    implementa remocao de evento no repository

--- src/repositories/event-repository.ts
+++ src/repositories/event-repository.ts
@@ -1,6 +1,6 @@
 import Member from '@base/entities/member'
 import IRepository from './repository'
-import { DTOEvent, DTOFilter, DTOLoginMember, DTOMember, DTORepositoryResult } from '@base/dtos'
+import { DTOEvent, DTOFilter, DTORepositoryResult } from '@base/dtos'
 import IDatabase from '@base/infra/idatabase'
 
 export default class EventRepository implements IRepository {
@@ -11,8 +11,8 @@ export default class EventRepository implements IRepository {
   async save (data: DTOEvent): Promise<DTORepositoryResult> {
     return this.database.save(this.collectionName, data)
   }
-  async remove(id: string): Promise<DTORepositoryResult> {
-    return Promise.resolve({ id, removed: true })
+  async remove(id: string, filter?: Object): Promise<DTORepositoryResult> {
+    return this.database.remove(this.collectionName, id, filter)
   }
   async update(data: Member): Promise<DTORepositoryResult> {
     return Promise.resolve({ id: data.id, updated: true })
