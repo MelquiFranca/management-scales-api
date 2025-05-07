98143e80879b9dcb6beab601a2fa8467666d2dc1
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-05-07 16:24:45 -0300

    atualiza documentacao api

--- src/services/remove-member-service.ts
+++ src/services/remove-member-service.ts
@@ -1,9 +1,9 @@
-import { DTORepositoryResult } from '@base/dtos'
+import { DTOFilter, DTORepositoryResult } from '@base/dtos'
 import IRepository from '@base/repositories/repository'
 
 export default class RemoveMemberService {
   constructor (private readonly repository: IRepository) {}
-  async execute (id: string): Promise<DTORepositoryResult> {
-    return this.repository.remove(id)
+  async execute (id: string, filter?: DTOFilter): Promise<DTORepositoryResult> {
+    return this.repository.remove(id, filter)
   }
 }
