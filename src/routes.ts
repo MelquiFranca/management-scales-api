8612a88aa5daf57777f184c2ce017e9f66ed90ce
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-05-07 17:23:36 -0300

    implementa rotas de grupos e atualiza documentacao api

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import IDatabase from './infra/idatabase'
 import LoginController from './controllers/login-controller'
 import { validate } from './middlewares/authenticator'
 import EventController from './controllers/event-controller'
+import GroupController from './controllers/group-controller'
 
 export default function (app: Express, database: IDatabase) {
   const router = Router()
@@ -14,12 +15,19 @@ export default function (app: Express, database: IDatabase) {
   routerWithAuthenticator.use(validate)
   const memberControler = new MemberController(database)
   const eventController = new EventController(database)
+  const groupController = new GroupController(database)
+
   routerWithAuthenticator.post(`/${eventController.path}`, eventController.create.bind(eventController))
   routerWithAuthenticator.get(`/${eventController.path}`, eventController.list.bind(eventController))
   routerWithAuthenticator.delete(`/${eventController.path}`, eventController.remove.bind(eventController))
+  
+  routerWithAuthenticator.post(`/${groupController.path}`, groupController.create.bind(groupController))
+  routerWithAuthenticator.get(`/${groupController.path}`, groupController.list.bind(groupController))
+  routerWithAuthenticator.delete(`/${groupController.path}`, groupController.remove.bind(groupController))
 
   routerWithAuthenticator.post(`/${memberControler.path}`, memberControler.create.bind(memberControler))
   routerWithAuthenticator.get(`/${memberControler.path}`, memberControler.list.bind(memberControler))
   routerWithAuthenticator.delete(`/${memberControler.path}`, memberControler.remove.bind(memberControler))
+
   app.use(routerWithAuthenticator)
 }
