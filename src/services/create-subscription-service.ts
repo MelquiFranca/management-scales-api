97bbfb0b0a0fc8fec0e95c927d8968f6c3bea134
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-05-09 14:33:18 -0300

    implementa service para criacao de subscription

---
+++ src/services/create-subscription-service.ts
@@ -0,0 +1,13 @@
+import { DTOEvent, DTORepositoryResult, DTOSubscription } from '@base/dtos'
+import IRepository from '@base/repositories/repository'
+
+export default class CreateSubscriptionService {
+  constructor (private readonly repository: IRepository) {}
+  async execute (data: DTOSubscription): Promise<DTORepositoryResult> {
+    if (!data.endpoint) throw new Error('Invalid subscription: endpoint')
+    if (!Number.isInteger(data.expirationTime)) throw new Error('Invalid subscription: expirationTime')
+    if (!data.userId) throw new Error('Invalid subscription: userId')
+    if (!data.keys?.auth || !data.keys?.p256dh) throw new Error('Invalid subscription: keys')
+    return this.repository.save(data)
+  }
+}
