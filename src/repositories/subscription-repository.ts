f5efb42415fac6d48efd6694e8928308d624ee76
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-05-09 14:32:58 -0300

    cria repository para subscription

---
+++ src/repositories/subscription-repository.ts
@@ -0,0 +1,26 @@
+import IRepository from './repository'
+import { DTOSubscription, DTOFilter, DTORepositoryResult } from '@base/dtos'
+import IDatabase from '@base/infra/idatabase'
+import Subscription from '@base/entities/subscription'
+
+export default class SubscriptionRepository implements IRepository {
+  constructor (
+    private readonly database: IDatabase,
+    private readonly collectionName: string = 'subscriptions'
+  ) {}
+  async save (data: DTOSubscription): Promise<DTORepositoryResult> {
+    return this.database.save(this.collectionName, data)
+  }
+  async remove(id: string, filter?: Object): Promise<DTORepositoryResult> {
+    return this.database.remove(this.collectionName, id, filter)
+  }
+  async update(data: Subscription): Promise<DTORepositoryResult> {
+    return Promise.resolve({ id: data.id, updated: true })
+  }
+  async list<DTOSubscription>(filter: DTOFilter): Promise<DTOSubscription[]> {
+    return this.database.list<DTOSubscription>(this.collectionName, filter)
+  }
+  async findByQuery<DTOSubscription>(filter: DTOFilter, options?: Object): Promise<DTOSubscription|null> {
+    return this.database.find<DTOSubscription|null>(this.collectionName, filter, options)
+  }
+}
