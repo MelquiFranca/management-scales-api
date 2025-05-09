be52799747fc455a1e5fb6240b09b532d3ca6224
Melquisedeque Pereira França <melquifranca@hotmail.com>
2025-05-09 14:33:44 -0300

    adiciona criacao de subscription no controller da rota de login

--- src/controllers/login-controller.ts
+++ src/controllers/login-controller.ts
@@ -2,26 +2,38 @@ import IDatabase from '@base/infra/idatabase'
 import { authenticate } from '@base/middlewares/authenticator'
 import MemberRepository from '@base/repositories/member-repository'
 import IRepository from '@base/repositories/repository'
+import SubscriptionRepository from '@base/repositories/subscription-repository'
+import CreateSubscriptionService from '@base/services/create-subscription-service'
 import ValidatePasswordMemberService from '@base/services/validate-password-member-service'
 import { Request, Response } from 'express'
 
 export default class LoginController {
   #path = 'login'
-  #repository: IRepository
+  #repositoryMember: IRepository
+  #repositorySubscription: IRepository
   constructor(database: IDatabase) {
-    this.#repository = new MemberRepository(database)
+    this.#repositoryMember = new MemberRepository(database)
+    this.#repositorySubscription = new SubscriptionRepository(database)
   }
   async login (req: Request, res: Response) {
     const { body } = req
     try {
       if (!body.username?.length) throw new Error('Invalid username')
       if (!body.password?.length) throw new Error('Invalid password')
-      const validatePasswordService = new ValidatePasswordMemberService(this.#repository)
-      const result = await validatePasswordService.execute({
+      const validatePasswordService = new ValidatePasswordMemberService(this.#repositoryMember)
+      const validatedPassword = await validatePasswordService.execute({
         password: body.password,
         username: body.username,
       })
-      const tokenResult = authenticate(result)
+      const tokenResult = authenticate(validatedPassword)
+      if (body.subscription) {
+        const createSubscription = new CreateSubscriptionService(this.#repositorySubscription)
+        await createSubscription.execute({
+          ...body.subscription,
+          userId: validatedPassword._id,
+          groupId: validatedPassword.groupId
+        })
+      }
       res.json(tokenResult)
     } catch (error) {
       console.error(error)
